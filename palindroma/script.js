895a004358df0622f70fb70a2a49e508d1d5f4ff
saracassandra7 <saracassandra7@gmail.com>
2022-10-19 18:13:12 +0200

    [add] palindrome exercise

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,1 +1,20 @@
-console.log('ciao');
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/ 
+
+const parolaInserita = prompt('Inserisci una parola');
+invertiParola(parolaInserita);
+
+let parolaInversa = invertiParola(parolaInserita);
+
+if(parolaInserita == parolaInversa){
+  document.querySelector('h1').innerText = `La parola "${parolaInserita}" è palindroma`  
+  console.log('la parola è palindroma');
+  } else {
+    document.querySelector('h1').innerText = `La parola "${parolaInserita}" NON è palindroma`
+    console.log('la parola non è palindroma');
+  }
+  
+ function invertiParola(parola){
+  let strInversa = parola.split('').reverse().join('');  
+  return strInversa;
+}
